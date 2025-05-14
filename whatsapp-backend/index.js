862cbeda2022bf1d0789b01296d0742e756a9afa
Sahar Alwakily <33603273+Sahar-Alwakily@users.noreply.github.com>
2025-05-14 22:19:31 +0300

    s7r!

--- whatsapp-backend/index.js
+++ whatsapp-backend/index.js
@@ -134,8 +134,8 @@ async function restoreScheduledJobs() {
 
   if (snapshot.exists()) {
     Object.entries(snapshot.val()).forEach(([jobId, jobData]) => {
-    console.log('📅 نوع التذكير:', timeUntilAppointment <= twentyFourHours ? 'تذكير قبل ساعة' : 'تذكير قبل 24 ساعة' );
-        if (jobData.status === 'scheduled' && new Date(jobData.sendAt) > new Date()) {
+      console.log('⏰ تشغيل التذكير:', jobId, new Date().toISOString());
+      if (jobData.status === 'scheduled' && new Date(jobData.sendAt) > new Date()) {
         const job = schedule.scheduleJob(new Date(jobData.sendAt), async () => {
           try {
                 await client.messages.create({
